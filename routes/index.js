2d593c135906f31167eef64f3fcbe82318fb2a33
jesus <jesusm.ramos@outlook.com>
2015-07-11 10:36:35 -0700

    Primera pregunta Linux

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require ('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+/* LLamada al controlador para mostrar la pregunta y la respuesta. */
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
