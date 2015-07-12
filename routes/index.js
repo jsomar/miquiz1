bdf9cec680754e6e424ce0c36e2ca16373e24966
jesus <jesusm.ramos@outlook.com>
2015-07-12 11:04:38 -0700

    Creditos en Linux

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,14 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET author */
+router.get('/author', function(req, res){
+	res.render('author', {credito: 'Autor'});
+});
+
 /* LLamada al controlador para mostrar la pregunta y la respuesta. */
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+/*router.get ('/author/author', quizController.author)*/
 
 module.exports = router;
