75fc637003172b16a01a033ed3df5f04c44d6c79
jesus <jesusm.ramos@outlook.com>
2015-07-26 10:07:47 -0700

    BBDD_1

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,21 @@
+var models = require('../models/models.js');
+
 /* GET /quizes/question */
 
 exports.question = function (req,res){
-	res.render ('quizes/question', {pregunta: '¿Cuál es la capital de Italia?'});
+	models.Quiz.findAll().success(function(quiz){
+		res.render ('quizes/question', {pregunta: quiz[0].pregunta})
+	})
 };
 
 /* GET /quizes/answer */
 
 exports.answer = function (req,res){
-	if (req.query.respuesta === 'Roma' || req.query.respuesta === 'roma'){
+	models.Quiz.findAll().success(function(quiz) {
+	if (req.query.respuesta === quiz[0].respuesta) {
 		res.render('quizes/answer', {respuesta: "Correcto"});
-	}else {
+	} else {
 		res.render('quizes/answer', {respuesta: "Incorrecto"});
 	}
+  })
 };
