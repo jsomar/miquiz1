19fb14d994596cf95349e611f0cf120273d2f4b1
jesus <jesusm.ramos@outlook.com>
2015-07-26 12:35:46 -0700

    Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,10 +12,12 @@ exports.question = function (req,res){
 
 exports.answer = function (req,res){
 	models.Quiz.findAll().success(function(quiz) {
+
 	if (req.query.respuesta === quiz[0].respuesta) {
 		res.render('quizes/answer', {respuesta: "Correcto"});
 	} else {
 		res.render('quizes/answer', {respuesta: "Incorrecto"});
 	}
+
   })
 };
--- models/models.js
+++ models/models.js
@@ -1,14 +1,33 @@
 var path = require ('path');
 
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\/(.*)/);
+var DB_name 	= (url[6]||null);
+var user 		= (url[2]||null);
+var pwd 		= (url[3]||null);
+var protocol 	= (url[1]||null);
+var dialect 	= (url[1]||null);
+var port 		= (url[5]||null);
+var host 		= (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
 // Cargar Modelo ORM
 var Sequelize = require ('sequelize');
 
-// Usar BBDD SQLite:
-var sequelize = new Sequelize(null, null, null,
-						 {dialect: "sqlite", storage: "quiz.sqlite"}
-						 );
+// Usar BBDD SQLite o Postgre:
+var sequelize = new Sequelize(DB_name, user, pwd,
+						 {dialect: protocol,
+						  protocol: protocol,
+						  port: port,
+						  host: host,
+						  storage: storage, //solo SQLite (.env)
+						  omitNull: true 
+						}
+					);
 // Importar la definición de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
 
@@ -19,6 +38,12 @@ sequelize.sync().success(function(){
 		if (count === 0) { // la tabla se inicializa solo si está vacia
 			Quiz.create({ pregunta: '¿Capital de Italia?',
 						  respuesta: 'Roma'
+						});
+			Quiz.create({ pregunta: '¿Quien descubrió América?',
+						  respuesta: 'Cristobal Colón'
+						});
+			Quiz.create({ pregunta: '¿Capital de Portugal?',
+						  respuesta: 'Lisboa'
 						})
 			.success(function(){console.log('Base de datos inicializada')});
 		};
